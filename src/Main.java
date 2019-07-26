ebcd34a60f4c256e840b31797ca4574acb4f1676
9elight <53308315+9elight@users.noreply.github.com>
2019-07-26 21:50:33 +0600

    исправленный

--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,20 @@ public class Main {
     public static void main(String[] args) {
         String[] name = {"Tony Stark", "Stive Rogers", "Natasha Romonov"};
 
+        for (int i=0; i<=2;i++) {
+            switch (name[i]) {
+                case "Tony Stark":
+                    System.out.println("Доброе утро " + name[0]);
+                    break;
+                case "Stive Rogers":
+                    System.out.println("Добрый день " + name[1]);
+                    break;
+                case "Natasha Romonov":
+                    System.out.println("Добрый вечер " + name[2]);
+                    break;
+            }
+        }
 
-        switch (name[0]) {
-            case "Tony Stark":
-                System.out.println("Доброе утро " + name[0]);
-                break;
-            case "Stive Rogers":
-                System.out.println("Добрый день " + name[1]);
-                break;
-            case "Natasha Romonov":
-                System.out.println("Добрый вечер " + name[2]);
-                break;
-        }
-        switch (name[1]) {
-            case "Tony Stark":
-                System.out.println("Доброе утро " + name[0]);
-                break;
-            case "Stive Rogers":
-                System.out.println("Добрый день " + name[1]);
-                break;
-            case "Natasha Romonov":
-                System.out.println("Добрый вечер " + name[2]);
-                break;
-        }
-        switch (name[2]) {
-            case "Tony Stark":
-                System.out.println("Доброе утро " + name[0]);
-                break;
-            case "Stive Rogers":
-                System.out.println("Добрый день" + name[1]);
-                break;
-            case "Natasha Romonov":
-                System.out.println("Добрый вечер " + name[2]);
-                break;
-        }
 
 
         // Задание на сообразительность
