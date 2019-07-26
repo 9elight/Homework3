6e3b5a917fca0ebd3eadc4644af4c0362483ff0d
9elight <53308315+9elight@users.noreply.github.com>
2019-07-26 18:20:40 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] name = {"Tony Stark", "Stive Rogers", "Natasha Romonov"};
+
+
+        switch (name[0]) {
+            case "Tony Stark":
+                System.out.println("Доброе утро " + name[0]);
+                break;
+            case "Stive Rogers":
+                System.out.println("Добрый день " + name[1]);
+                break;
+            case "Natasha Romonov":
+                System.out.println("Добрый вечер " + name[2]);
+                break;
+        }
+        switch (name[1]) {
+            case "Tony Stark":
+                System.out.println("Доброе утро " + name[0]);
+                break;
+            case "Stive Rogers":
+                System.out.println("Добрый день " + name[1]);
+                break;
+            case "Natasha Romonov":
+                System.out.println("Добрый вечер " + name[2]);
+                break;
+        }
+        switch (name[2]) {
+            case "Tony Stark":
+                System.out.println("Доброе утро " + name[0]);
+                break;
+            case "Stive Rogers":
+                System.out.println("Добрый день" + name[1]);
+                break;
+            case "Natasha Romonov":
+                System.out.println("Добрый вечер " + name[2]);
+                break;
+        }
+
+
+        // Задание на сообразительность
+
+        name = Arrays.copyOf(name,4);
+        name[3]="Bruce Banner";
+        for (int i=0; i<=3;i++){
+            switch (name[i]){
+                case "Tony Stark":
+                    System.out.println("Доброе утро " + name[0]);
+                    break;
+                case "Stive Rogers":
+                    System.out.println("Добрый день " + name[1]);
+                    break;
+                case "Natasha Romonov":
+                    System.out.println("Добрый вечер " + name[2]);
+                    break;
+                case "Bruce Banner":
+                    System.out.println("Доброй ночи " + name[3]);
+            }
+        }
+
+
+
+    }
+}
